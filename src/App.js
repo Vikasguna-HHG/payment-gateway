f9709d9b0cf6343d464b3033c5d213947bfaf47b
Vikasguna-HHG <hhgsoftechteam10@gmail.com>
2023-02-28 18:28:51 +0530

    payment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Stripe from './Stripe/Stripe'
+import Razorpay from './Razorpay/Razorpay'
+import CheckoutForm from './Stripe/CheckoutForm';
+import PayPal from './PayPal/PayPal';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>----------stripe----------</p>
+      <Stripe />
+      <p>----------Razorpay----------</p>
+      <Razorpay />
+      {/* <CheckoutForm /> */}
+      <p>----------PayPal----------</p>
+      <PayPal />
     </div>
   );
 }
